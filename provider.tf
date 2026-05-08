9646e9cc43a18348dc5cdcd5e19146e9ef53b57a
LabeeqQ <labeeqqazi@gmail.com>
2026-05-08 20:38:10 +0000

    Configuring Remote Backend

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+
+  backend "s3" {
+    bucket         = "labeeq-terraform-state-demo"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
